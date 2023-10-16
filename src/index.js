71463222730429bfe273463f4233b34265adb32c
naetsawd <dechsitbkk@gmail.com>
2023-10-16 01:36:33 -0400

    Added unit and daily/hourly forecast toggles.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,38 @@ const speedUnitItems = [
     { class: "miles", text: "mi" }
 ];
 
+const forecastOpt = [
+    { class: "daily", text: "Daily" },
+    { class: "hourly", text: "Hourly" }
+];
+
 document.addEventListener("DOMContentLoaded", function() {
+    if (!localStorage.getItem("city")) {
+        localStorage.setItem("city", "toronto")
+    }
+
+    if (!localStorage.getItem("tempUnit")) {
+        localStorage.setItem("tempUnit", "°C")
+    }
+
+    if (!localStorage.getItem("speedUnit")) {
+        localStorage.setItem("speedUnit", "km")
+    }
+
+    if (!localStorage.getItem("forecast")) {
+        localStorage.setItem("forecast", "daily")
+    }
+
+    createNavItem("forecastOpt", forecastOpt);
     createNavItem("tempUnit", tempUnitItems);
     createNavItem("speedUnit", speedUnitItems);
-    makeCurrInfo();
-    makeForecast();
+    makeCurrInfo(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("speedUnit"));
+    makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    searchFunction();
+    switchForecast();
+    switchTemp();
+    switchSpeed();
+    chosenUnit();
 });
 
 function createNavItem(container, items) {
@@ -39,143 +66,333 @@ function createNavItem(container, items) {
     document.querySelector("nav").append(ul);
 }
 
-function fetchLeftInfo() {
-    return fetch("http://api.weatherapi.com/v1/forecast.json?key=14f6e670b9ca46fcaf2203655231110&q=Toronto&days=1&aqi=no&alerts=no", {mode: 'cors'})
+function chosenUnit() {
+    const units = document.querySelectorAll(".navItem");
+
+    units.forEach((unit) => {
+        unit.classList.remove("chosenUnit");
+    })
+
+    if (localStorage.getItem("forecast") === "daily") {
+        units[0].classList.add("chosenUnit")
+    } else {
+        units[1].classList.add("chosenUnit")
+    }
+
+    if (localStorage.getItem("tempUnit") === "°C") {
+        units[2].classList.add("chosenUnit")
+    } else {
+        units[3].classList.add("chosenUnit")
+    }
+
+    if (localStorage.getItem("speedUnit") === "km") {
+        units[4].classList.add("chosenUnit")
+    } else {
+        units[5].classList.add("chosenUnit")
+    }
+}
+
+async function searchFunction() {
+    const content = document.querySelector(".content");
+    const searchBox = document.querySelector(".searchBox");
+    const searchBtn = document.querySelector(".searchBtn");
+
+    searchBtn.onclick = async function() {
+        if (searchBox.value.trim()) {
+            localStorage.setItem("city", searchBox.value);
+
+            content.innerHTML = '';
+            searchBox.value = "";
+            await makeCurrInfo(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("speedUnit"));
+            await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));  
+        }
+    }
+}
+
+async function switchForecast() {
+    const forecastSection = document.querySelector(".forecastSection");
+    const daily = document.querySelector(".daily");
+    const hourly = document.querySelector(".hourly");
+
+    daily.onclick = async function() {
+        localStorage.setItem("forecast", "daily");
+
+        forecastSection.innerHTML = '';
+        chosenUnit();
+        await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    }
+
+    hourly.onclick = async function() {
+        localStorage.setItem("forecast", "hourly");
+
+        forecastSection.innerHTML = '';
+        chosenUnit();
+        await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    }
+}
+
+async function switchTemp() {
+    const content = document.querySelector(".content");
+    const C = document.querySelector(".celsius");
+    const F = document.querySelector(".fahrenheit");
+
+    C.onclick = async function() {
+        localStorage.setItem("tempUnit", "°C");
+
+        content.innerHTML = '';
+        chosenUnit();
+        await makeCurrInfo(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("speedUnit"));
+        await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    }
+
+    F.onclick = async function() {
+        localStorage.setItem("tempUnit", "°F");
+
+        content.innerHTML = '';
+        chosenUnit();
+        await makeCurrInfo(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("speedUnit"));
+        await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    }
+}
+
+async function switchSpeed() {
+    const content = document.querySelector(".content");
+    const KM = document.querySelector(".kilometers");
+    const MI = document.querySelector(".miles");
+
+    KM.onclick = async function() {
+        localStorage.setItem("speedUnit", "km");
+
+        content.innerHTML = '';
+        chosenUnit();
+        await makeCurrInfo(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("speedUnit"));
+        await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    }
+
+    MI.onclick = async function() {
+        localStorage.setItem("speedUnit", "mi");
+
+        content.innerHTML = '';
+        chosenUnit();
+        await makeCurrInfo(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("speedUnit"));
+        await makeForecast(localStorage.getItem("city"), localStorage.getItem("tempUnit"), localStorage.getItem("forecast"));
+    }
+}
+
+function fetchRequest(city) {
+    try {
+        return fetch("http://api.weatherapi.com/v1/forecast.json?key=14f6e670b9ca46fcaf2203655231110&q=" + city + "&days=7&aqi=no&alerts=no", {mode: 'cors'})
         .then(function(response) {
             return response.json();
         })
-        .then(function(response) {
-            const localTimeEpoch = response.location.localtime_epoch * 1000;
-            const localTimeDate = new Date(localTimeEpoch);
-            
-            const formattedDate = localTimeDate.toLocaleDateString('en-US', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-            });
-
-            const formattedTime = localTimeDate.toLocaleTimeString('en-US', {
-                hour: '2-digit',
-                minute: '2-digit',
-                timeZoneName: 'short'
-            });
-
-            return [
-                { class: "cityName", text: response.location.name + ", " + response.location.country },
-                { class: "cityDate", text: formattedDate },
-                { class: "cityTime", text: formattedTime },
-                { class: "cityTemp", text: response.current.temp_c + "°C | Feels Like " + response.current.feelslike_c + "°C" },
-                { class: "cityWeather", text: response.current.condition.text },
-                { class: "weatherIcon", icon: response.current.condition.icon }
-            ];
-        });
+    } catch(error) {
+        alert("Please enter a valid city name.")
+    }
 }
 
-function fetchRightInfo() {
-    return fetch("http://api.weatherapi.com/v1/forecast.json?key=14f6e670b9ca46fcaf2203655231110&q=Toronto&days=1&aqi=no&alerts=no", {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            return [
-                { class: "rightInfoTitle", text: "Humidity" },
-                { class: "rightInfoNo", text: response.current.humidity + " % | " + "High" },
-                { class: "rightInfoTitle", text: "Chance of Rain" },
-                { class: "rightInfoNo", text: response.forecast.forecastday[0].day.daily_will_it_rain + " % | " + "None" },
-                { class: "rightInfoTitle", text: "Chance of Snow" },
-                { class: "rightInfoNo", text: response.forecast.forecastday[0].day.daily_will_it_snow + " % | " + "None" },
-                { class: "rightInfoTitle", text: "Wind Speed" },
-                { class: "rightInfoNo", text: response.current.wind_kph + " km/h | " + "Strong" }
-            ];
+function fetchLeftInfo(city, tempUnit) {
+    return fetchRequest(city).then(function(response) {
+        const localTimeEpoch = response.location.localtime_epoch * 1000;
+        const localTimeDate = new Date(localTimeEpoch);
+        
+        const formattedDate = localTimeDate.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
         });
+
+        const formattedTime = localTimeDate.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZoneName: 'short'
+        });
+
+        let tempVal = response.current.temp_c;
+        let feelsVal = response.current.feelslike_c;
+        
+        if (tempUnit === "°F") {
+            tempVal = response.current.temp_f;
+            feelsVal = response.current.feelslike_f;
+        };
+
+        return [
+            { class: "cityName", text: response.location.name + ", " + response.location.country },
+            { class: "cityDate", text: formattedDate },
+            { class: "cityTime", text: formattedTime },
+            { class: "cityTemp", text: tempVal + tempUnit + " | Feels Like " + feelsVal + tempUnit },
+            { class: "cityWeather", text: response.current.condition.text },
+            { class: "weatherIcon", icon: response.current.condition.icon }
+        ];
+    });
 }
 
-function makeCurrInfo() {
+function fetchRightInfo(city, speed) {
+    return fetchRequest(city).then(function(response) {
+        let speedVal = response.current.wind_kph;
+        
+        if (speed === "mi") {
+            speedVal = response.current.wind_mph;
+        };
+
+        return [
+            { class: "rightInfoTitle", text: "Humidity" },
+            { class: "rightInfoNo", text: response.current.humidity + " %" },
+            { class: "rightInfoTitle", text: "Chance of Rain" },
+            { class: "rightInfoNo", text: response.forecast.forecastday[0].day.daily_will_it_rain + " %" },
+            { class: "rightInfoTitle", text: "Chance of Snow" },
+            { class: "rightInfoNo", text: response.forecast.forecastday[0].day.daily_will_it_snow + " %" },
+            { class: "rightInfoTitle", text: "Wind Speed" },
+            { class: "rightInfoNo", text: speedVal + " " + speed + "/h" }
+        ];
+    });
+}
+
+function makeCurrInfo(city, tempUnit, speed) {
     const content = document.querySelector(".content");
 
     const currInfo = document.createElement("section");
     currInfo.classList.add("currInfo");
 
+    currInfo.innerHTML = ``;
+
     const currInfoLeft = document.createElement("section");
     currInfoLeft.classList.add("currInfoLeft");
 
-    fetchLeftInfo().then(function(result) {
-        result.forEach((item) => {
-            const textContainer = document.createElement("div");
-            textContainer.classList.add(item.class);
+    return Promise.all([fetchLeftInfo(city, tempUnit), fetchRightInfo(city, speed)])
+        .then(function (results) {
+            const leftInfoResult = results[0];
+            const rightInfoResult = results[1];
 
-            const iconContainer = document.createElement("img");
-            iconContainer.classList.add(item.class);
-    
-            if (item.icon) {
-                iconContainer.src = item.icon;
+            leftInfoResult.forEach((item) => {
+                const textContainer = document.createElement("div");
+                textContainer.classList.add(item.class);
 
-                currInfoLeft.append(iconContainer);
-            } else {
+                const iconContainer = document.createElement("img");
+                iconContainer.classList.add(item.class);
+
+                if (item.icon) {
+                    iconContainer.src = item.icon;
+
+                    currInfoLeft.append(iconContainer);
+                } else {
+                    textContainer.textContent = item.text;
+
+                    currInfoLeft.append(textContainer);
+                }
+            });
+
+            const currInfoRight = document.createElement("section");
+            currInfoRight.classList.add("currInfoRight");
+
+            rightInfoResult.forEach((item) => {
+                const textContainer = document.createElement("div");
+                textContainer.classList.add(item.class);
                 textContainer.textContent = item.text;
 
-                currInfoLeft.append(textContainer);
-            }
-        });
-    });    
-    
-    const currInfoRight = document.createElement("section");
-    currInfoRight.classList.add("currInfoRight");
+                currInfoRight.append(textContainer);
+            });
 
-    fetchRightInfo().then(function(result) {
-        result.forEach((item) => {
-            const textContainer = document.createElement("div");
-            textContainer.classList.add(item.class);
-            textContainer.textContent = item.text;
-
-            currInfoRight.append(textContainer);
-        });
-    });    
-
-    currInfo.append(currInfoLeft, currInfoRight);
-    content.append(currInfo);
-}
-
-function fetchForecast(num) {
-    return fetch("http://api.weatherapi.com/v1/forecast.json?key=14f6e670b9ca46fcaf2203655231110&q=Toronto&days=7&aqi=no&alerts=no", {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            let forecastData = response.forecast.forecastday;
-
-            return [
-                { class: "forecastDate", text: forecastData[num].date },
-                { class: "forecastMaxTemp", text: forecastData[num].day.maxtemp_c },
-                { class: "forecastMinTemp", text: forecastData[num].day.mintemp_c },
-                { class: "forecastWeather", text: forecastData[num].day.condition.text },
-            ];
+            currInfo.append(currInfoLeft, currInfoRight);
+            content.append(currInfo);
         });
 }
 
-function makeForecast() {
+function fetchForecastDaily(num, city, tempUnit) {
+    return fetchRequest(city).then(function(response) {
+        let forecastData = response.forecast.forecastday;
+
+        const localTimeDate = new Date(forecastData[num].date);
+
+        const formattedDate = localTimeDate.toLocaleDateString('en-US', {
+            weekday: 'short',
+            month: 'short',
+            day: 'numeric',
+        });
+
+        let tempValMax = forecastData[num].day.maxtemp_c;
+        let tempValMin = forecastData[num].day.mintemp_c
+        
+        if (tempUnit === "°F") {
+            tempValMax = forecastData[num].day.maxtemp_f;
+            tempValMin = forecastData[num].day.mintemp_f;
+        };
+
+        return [
+            { class: "forecastDate", text: formattedDate },
+            { class: "forecastMaxTemp", text: "High: " + tempValMax + tempUnit },
+            { class: "forecastMinTemp", text: "Low: " + tempValMin + tempUnit },
+            { class: "forecastWeather", text: forecastData[num].day.condition.text },
+        ];
+    });
+}
+
+function fetchForecastHourly(num, city, tempUnit) {
+    return fetchRequest(city).then(function(response) {
+        let forecastData = response.forecast.forecastday[0].hour[num];
+
+        const localTimeEpoch = forecastData.time_epoch * 1000;
+
+        const localTimeDate = new Date(localTimeEpoch);
+
+        const formattedDate = localTimeDate.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+        });
+
+        let tempVal = forecastData.temp_c;
+        
+        if (tempUnit === "°F") {
+            tempVal = forecastData.temp_f;
+        };
+
+        return [
+            { class: "forecastDate", text: formattedDate },
+            { class: "forecastMaxTemp", text: "Temp: " + tempVal + tempUnit },
+            { class: "forecastWeather", text: forecastData.condition.text },
+        ];
+    });
+}
+
+function makeForecast(city, tempUnit, type) {
     const content = document.querySelector(".content");
 
-    const forecastSection = document.createElement("section");
-    forecastSection.classList.add("forecastSection");
-
-    for (let i = 1; i < 7; i++) {
-        const forecastCard = document.createElement("div");
-        forecastCard.classList.add("forecastCard");
-        
-        fetchForecast(i).then(function(result) {
-            result.forEach((item) => {
-                const forecastData = document.createElement("div");
-                forecastData.classList.add(item.class);
-                forecastData.textContent = item.text;
-
-                forecastCard.append(forecastData);
-            });
-        })
-
-        forecastSection.append(forecastCard);
+    let forecastSection = document.querySelector(".forecastSection");
+    if (!forecastSection) {
+        forecastSection = document.createElement("section");
+        forecastSection.classList.add("forecastSection");
+        content.append(forecastSection);
     }
 
-    content.append(forecastSection);
+    forecastSection.innerHTML = ``;
+
+    const fetchPromises = [];
+    if (type === "daily") {
+        for (let i = 1; i < 7; i++) {
+            fetchPromises.push(fetchForecastDaily(i, city, tempUnit));
+        }
+    } else {
+        for (let i = 1; i < 24; i++) {
+            fetchPromises.push(fetchForecastHourly(i, city, tempUnit));
+        }
+    }
+
+    return Promise.all(fetchPromises)
+        .then((results) => {
+            results.forEach((forecastData) => {
+                const forecastCard = document.createElement("div");
+                forecastCard.classList.add("forecastCard");
+
+                forecastData.forEach((item) => {
+                    const forecastItem = document.createElement("div");
+                    forecastItem.classList.add(item.class);
+                    forecastItem.textContent = item.text.replace(" possible", "");
+
+                    forecastCard.append(forecastItem);
+                });
+
+                forecastSection.append(forecastCard);
+            });
+
+            content.append(forecastSection);
+        });
 }
